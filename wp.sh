2e4f880deab80741201db24ee0f75d25a9bb973e
pietromleite <101213536+pietromleite@users.noreply.github.com>
2022-03-10 14:05:10 -0300

    Update wp.sh

--- wp.sh
+++ wp.sh
@@ -62,7 +62,9 @@ sleep 1
 echo " ================================================================== "
 echo " Configurando Ngix "
 echo " ================================================================== "
-#sudo chown ec2-user:ec2-user /home/ec2-user -R
+sudo chown ec2-user:ec2-user /home/ec2-user -R
+sudo chown ec2-user:ec2-user /home/ec2-user/wordpress-docker/ -R
+cd wordpress-docker
 sudo curl -sSL  https://raw.githubusercontent.com/pietromleite/docker-compose/main/wordpress.conf > wordpress.conf
 echo " OK "
 sleep 1
